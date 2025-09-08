fca161c3959ba29bfbe8a8c8220893f0e7534fb1
Diego <dellagostino@gmail.com>
2025-09-08 13:49:25 -0300

    ajsute presented by

--- app/components/SponsorDisplay.tsx
+++ app/components/SponsorDisplay.tsx
@@ -5,52 +5,15 @@ import Image from 'next/image';
 
 export default function SponsorDisplay() {
   return (
-    <motion.div
-      initial={{ opacity: 0, x: 20 }}
-      animate={{ opacity: 1, x: 0 }}
-      transition={{ duration: 0.5, delay: 0.6 }}
-      className="sponsor-display"
-    >
+    <div className="sponsor-display">
       {/* Sponsor Separator */}
-      <motion.div 
-        className="sponsor-separator"
-        initial={{ scaleY: 0, opacity: 0 }}
-        animate={{ scaleY: 1, opacity: 1 }}
-        transition={{ 
-          duration: 0.5, 
-          delay: 0.5,
-          ease: [0.25, 0.46, 0.45, 0.94]
-        }}
-      ></motion.div>
+      <div className="sponsor-separator"></div>
       
-      <motion.div 
-        className="sponsor-content"
-        initial={{ opacity: 0, y: 10 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ 
-          duration: 0.4, 
-          delay: 0.6,
-          ease: [0.25, 0.46, 0.45, 0.94]
-        }}
-      >
-        <motion.div 
-          className="sponsor-text"
-          initial={{ opacity: 0, y: 5 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.3, delay: 0.7 }}
-        >
+      <div className="sponsor-content">
+        <div className="sponsor-text">
           Presented by
-        </motion.div>
-        <motion.div 
-          className="sponsor-logo"
-          initial={{ opacity: 0, scale: 0.9 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ 
-            duration: 0.5, 
-            delay: 0.8,
-            ease: [0.34, 1.56, 0.64, 1]
-          }}
-        >
+        </div>
+        <div className="sponsor-logo">
           <Image
             src="/xapologo.png"
             alt="XAPO BANK"
@@ -58,8 +21,8 @@ export default function SponsorDisplay() {
             height={80}
             className="xapo-logo"
           />
-        </motion.div>
-      </motion.div>
-    </motion.div>
+        </div>
+      </div>
+    </div>
   );
 }
